5c5aa08e55f56feb3bae5a7ed076ec6ba3ee1fe3
Sara Thornsberry <sarathornsberry@saras-mbp.home>
2020-05-15 00:05:04 -0400

    completed FizzBuzz and added tests

--- src/main/java/org/wecancodeit/fizzbuzz/FizzBuzz.java
+++ src/main/java/org/wecancodeit/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,12 @@
 package org.wecancodeit.fizzbuzz;
 
-
 public class FizzBuzz {
-}
+
+        public String translate(int numberToTranslate) {
+
+            if (numberToTranslate % 15 == 0) return "FizzBuzz";
+            else if (numberToTranslate % 3 == 0) return "Fizz";
+            else if (numberToTranslate % 5 == 0) return "Buzz";
+            else return "" + numberToTranslate;
+        }
+    }
--- src/test/java/org/wecancodeit/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/org/wecancodeit/fizzbuzz/FizzBuzzTest.java
@@ -1,10 +1,32 @@
 package org.wecancodeit.fizzbuzz;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
+    FizzBuzz fb = new FizzBuzz();
+
     @Test
-    public void shouldInstantiate() {
-        FizzBuzz underTest = new FizzBuzz();
+    public void returnsNumberIfNotMultiple() {
+        String output = fb.translate(1);
+        assertEquals("1", output);
+    }
+
+    @Test
+    public void returnsFizzIfMultipleOf3() {
+        String output = fb.translate(3);
+        assertEquals("Fizz", output);
+    }
+
+    @Test
+    public void returnsBuzzIfMultipleOf5() {
+        String output = fb.translate(5);
+        assertEquals("Buzz", output);
+    }
+
+    @Test
+    public void returnsFizzBuzzIfMultipleOf3And5() {
+        String output = fb.translate(15);
+        assertEquals("FizzBuzz", output);
     }
 }
